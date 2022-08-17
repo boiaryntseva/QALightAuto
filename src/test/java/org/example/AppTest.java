19e4d84ca6449286acf955ddf35809ffbbf2b70d
boiaryntseva <boiaryntseva@gmail.com>
2022-08-17 13:44:34 -0400

    1) Test cases are updated 2) WebDriver and DriverManager dependencies are added 3) Base test to open url is added

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,19 +2,32 @@ package org.example;
 
 import static org.junit.Assert.assertTrue;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
+    WebDriver webDriver;
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void openUrl()
     {
-        assertTrue( true );
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        webDriver.get("https://www.nyrr.org/");
+        System.out.println("Ready to run?");
+
+        webDriver.quit();
     }
 }
